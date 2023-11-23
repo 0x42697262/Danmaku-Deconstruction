d95abaca2f00b3bdd5983c88e6ce97b931a3dbcd
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-11-24 07:55:08 +0900

    added circle appearance for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,11 +18,24 @@ extends Area2D
 ## Graze collision shape 2d
 ##
 ## Used for detecting player collisions against another player.
-@export var graze_area: CollisionShape2D
+var graze_area: CollisionShape2D
 ## Player hitbox collision shape 2d
 ##
 ## Used for detecting collisions with a bullet.
-@export var hitbox: CollisionShape2D
+var hitbox: CollisionShape2D
+## Polygon body circle
+var body: Polygon2D
+
+## Player settings
+##
+## Allow to be change in multiplayer
+@export_group("Settings")
+## Player radius
+@export var body_radius: int = 14
+## Hitbox radius
+@export var hitbox_radius: float = 1.5
+## Player color
+@export var color: Color = Color.RED
 
 ## Player movement
 ##
@@ -49,7 +62,7 @@ func move(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed * speed_multiplier
-	
+		
 	position += velocity * delta
 
 	position.x = clamp(position.x, 0, 600)
@@ -58,18 +71,34 @@ func move(delta):
 ##
 ## Adds collision shape children and sprite.
 func _ready():
+	# add collisions
 	graze_area = CollisionShape2D.new()
 	hitbox = CollisionShape2D.new()
 	var graze_area_shape: CircleShape2D	= CircleShape2D.new()
 	var hitbox_shape: CircleShape2D	= CircleShape2D.new()
-	graze_area_shape.radius	= 14
-	hitbox_shape.radius	= 1.5
+	graze_area_shape.radius	= body_radius
+	hitbox_shape.radius	= hitbox_radius
 	graze_area.shape = graze_area_shape
 	hitbox.shape = hitbox_shape
 
 	add_child(graze_area)
 	add_child(hitbox)
 	
+	# add body
+	body = Polygon2D.new()
+	var polygon_array = PackedVector2Array()
+	for i in range(361):
+		var angle = deg_to_rad(i)
+		var point = Vector2(cos(angle), sin(angle)) * body_radius
+		polygon_array.push_back(point)
+	body.polygon = polygon_array
+	body.color = color
+
+	add_child(body)
+
+
+
+	# set default values
 	speed = 200
 	speed_multiplier = 1.0
 
