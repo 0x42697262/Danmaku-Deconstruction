75f409e134e084df0f9851512858ab300ce78f34
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-11-24 23:38:26 +0900

    added play area

---
+++ scripts/play_area_container.gd
@@ -0,0 +1,14 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var player = preload("res://scenes/player.tscn").instantiate()
+	add_child(player)
+	player.position = Vector2(0,632)
+	print(position)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
