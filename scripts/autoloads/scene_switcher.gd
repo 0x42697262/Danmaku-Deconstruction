18d4994c1d0503efbffc51a647fe7dc2f24dc3a7
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-11-26 02:59:05 +0900

    fix bug "_read()" to "_ready()"

--- scripts/autoloads/scene_switcher.gd
+++ scripts/autoloads/scene_switcher.gd
@@ -2,8 +2,9 @@ extends Node
 
 var list_of_scenes: Dictionary
 
-func _read():
-	list_of_scenes['gameplay'] = preload("res://scenes/world/gameplay.tscn")
+func _ready():
+	list_of_scenes["gameplay"] = preload("res://scenes/world/gameplay.tscn")
+
 
 func switch(scene: String):
 	get_tree().change_scene_to_packed(list_of_scenes[scene])
