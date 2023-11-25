5a2d9ef29df11a478774b897a37c69338677be17
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-11-26 02:40:46 +0900

    center scene position

--- scenes/main.gd
+++ scenes/main.gd
@@ -27,8 +27,16 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	set_scene_position()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+## Sets the position of scenes in game_world to center
+func set_scene_position():
+	var window_width: int = ProjectSettings.get_setting("display/window/size/viewport_width")
+	var window_height: int = ProjectSettings.get_setting("display/window/size/viewport_height")
+	var window_size: Vector2 = Vector2(window_width, window_height)
+
+	$game_world.set_position(window_size/2)
