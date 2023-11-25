4b97ec91cbaca04afe9ed9f914c7e31da14e03ab
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-11-26 02:52:02 +0900

    renamed func name

--- scripts/autoloads/scene_switcher.gd
+++ scripts/autoloads/scene_switcher.gd
@@ -5,5 +5,5 @@ var list_of_scenes: Dictionary
 func _read():
 	list_of_scenes['gameplay'] = preload("res://scenes/world/gameplay.tscn")
 
-func switch_scene(scene: String):
+func switch(scene: String):
 	get_tree().change_scene_to_packed(list_of_scenes[scene])
